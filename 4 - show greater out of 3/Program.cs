20fb1ebd1c77cb3e6829a38ca22352d7ea038f34
Dmitry Yarmiychuk <yarmiychuk@gmail.com>
2022-07-08 15:08:28 +0300

    Show Greater Out Of 3 program added

---
+++ 4 - show greater out of 3/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Напишите программу, которая принимает на вход три числа
+и выдаёт максимальное из этих чисел.
+*/
+
+Console.WriteLine("Программа для нахождения максимального из трёх целых чисел");
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+if(num2 > max)
+    max = num2;
+if(num3 > max)
+    max = num3;
+
+if (num1 != num2 || num2 != num3)
+    Console.WriteLine("Наибольшее число: " + max);
+else
+    Console.WriteLine("Введённые числа равны");
